fc9fd8bb4b2c75cd4d297d2542208ff518d3811c
Alexei Vinidiktov <alexei@vinidiktov.ru>
2014-06-21 22:45:40 +0700

    Initial check-in of module SpellingApp

---
+++ SpellingApp/SpellingApp/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellingApp
+{
+    class Card
+    {
+        public String uuid {get; set;}
+        public String question {get; set;}
+		public String answer {get; set;}
+		public String reading {get; set;}
+		public String context {get; set;}
+		//public var answerPicture:String;
+		//public var answerAudio:String;
+		public String questionAudio  {get; set;}
+        public int stage { get; set; }
+		//public var dueDate:Date;
+    }
+}
